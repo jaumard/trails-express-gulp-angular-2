009190b6ff1859913c30b98ac9a2c3d230a829c9
jaumard <ioquoo3H>
2016-05-17 17:36:17 +0200

    Fix rest api front problem

--- assets/app/hero.service.ts
+++ assets/app/hero.service.ts
@@ -5,27 +5,33 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-
+  baseUrl : string;
   constructor(private _http:Http) {
+    this.baseUrl = 'http://localhost:3000';
   }
 
   getHeroes() {
-    return this._http.get('http://localhost:3000/api/v1/hero').map(res => res.json()).toPromise();
+    return this._http.get(`${this.baseUrl}/api/v1/hero`).map(res => res.json()).toPromise();
   }
 
   getHero(id:number) {
-    return this._http.get('http://localhost:3000/api/v1/hero/' + id).map(res => res.json()).toPromise();
+    return this._http.get(`${this.baseUrl}/api/v1/hero/${id}`).map(res => res.json()).toPromise();
   }
 
   deleteHero(id:number) {
-    return this._http.delete('http://localhost:3000/api/v1/hero/' + id).map(res => res.json()).toPromise();
+    return this._http.delete(`${this.baseUrl}/api/v1/hero/${id}`).map(res => res.json()).toPromise();
   }
 
   saveHero(hero:Hero) {
     let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
     let options = new RequestOptions({headers: headers});
-    if (hero.id == -1) delete hero.id;
+    if (hero.id == -1){
+      delete hero.id;
+      return this._http.post(`${this.baseUrl}/api/v1/hero`, JSON.stringify(hero), options).map(res => res.json()).toPromise();
+    }
+    else {
+      return this._http.put(`${this.baseUrl}/api/v1/hero/${hero.id}`, JSON.stringify(hero), options).map(res => res.json()).toPromise();
+    }
 
-    return this._http.post('http://localhost:3000/api/v1/hero', JSON.stringify(hero), options).map(res => res.json()).toPromise();
   }
 }
